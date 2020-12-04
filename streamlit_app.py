ed7e5803a63346033d4b3a175cfd55e5277d65e0
Nihal D'Souza <nidsouza@cisco.com>
2020-12-05 01:26:03 +0530

    Changed for heroku

--- streamlit_app.py
+++ streamlit_app.py
@@ -82,14 +82,13 @@ def run_the_app(algorithm, params=None):
         st.title(subject)
         with st.spinner(text="Generating summary..."):
             st.markdown(get_main_summary(wc))
-        # st.success(text="Summary generated successfully")
         with st.spinner(text="Generating prerequisites..."):
             prereqs = get_prereq_summary(wc)
         st.header("Prerequisites")
         for subhead in prereqs:
             st.subheader(subhead)
             st.write(prereqs[subhead])
-        st.success("Prerequisites generated successfully")
+        st.success("Done!")
 
 
 @st.cache(show_spinner=False)
